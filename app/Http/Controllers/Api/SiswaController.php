92ae95aeddca5cc57d7e0dc068018db590306875
suryaoke <89033805+suryaoke@users.noreply.github.com>
2023-11-08 00:18:08 +0700

    lanjutan

--- app/Http/Controllers/Api/SiswaController.php
+++ app/Http/Controllers/Api/SiswaController.php
@@ -25,27 +25,28 @@ class SiswaController extends Controller
 
             // Find the corresponding student record with the provided user ID
             $ortu = OrangTua::where('id_user', $userId)->first();
-            $siswa = Siswa::where('id', $ortu->id_siswa)->get();
-            $siswaId = Siswa::where('id', $ortu->id_siswa)->first();
-            $rombelsiswa = Rombelsiswa::where('id_siswa', $siswaId->id)->first();
-            $rombel = Rombel::where('id', $rombelsiswa->id_rombel)->first();
-            $kelasId = Kelas::where('id', $rombel->id_kelas)->first();
-            $kelas = Kelas::where('id', $rombel->id_kelas)->get();
-            $jurusan = Jurusan::where('id', $kelasId->id_jurusan)->get();
-            $siswa = Siswa::where('id', $ortu->id_siswa)->get();
+            $siswa = Siswa::where('id', $ortu->id_siswa)->first();
+            $rombelsiswa = Rombelsiswa::where('id_siswa', $siswa->id)->first();
+            $siswa = Siswa::where('id', $rombelsiswa->id_siswa)->get();
 
-            if ($siswa) {
-                // If the student record exists, return it as a success response
+            $responseSiswa = [];
+            foreach ($siswa as  $siswaId) {
+                $rombelsiswaData = Rombelsiswa::where('id_siswa', $siswaId->id)->first();
+                $rombelData = Rombel::find($rombelsiswaData->id_rombel);
+                $kelasData = Kelas::find($rombelData->id_kelas);
+                $jurusanData = Jurusan::find($kelasData->id_jurusan);
 
-                return response()->json([
-                    'siswa' => $siswa,
-                    'kelas' => $kelas,
-                    'jurusan' => $jurusan,
-                ], 200);
-            } else {
-                // If the student record does not exist, return an appropriate error response
-                return ResponseFormatter::error('Student data not found', 404);
+
+                $siswaId->kelas = $kelasData;
+                $siswaId->jurusan =       $jurusanData;
+
+                $responseSiswa[] = $siswaId;
             }
+
+            return response()->json([
+                'siswa' => $responseSiswa,
+
+            ], 200);
         } catch (Exception $error) {
             // Catch any exceptions that might occur and return a general error message
             return ResponseFormatter::error('Failed to retrieve student data');
